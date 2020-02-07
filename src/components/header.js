f0d10049a4cbf73acaadfc1ec0dedbd11a2f4d3f
Shekhar Gurav <shekhar@Shekhars-MacBook-Pro.local>
2020-02-07 11:28:00 +0530

    header component added with all responsiveness

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,32 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+const Header = () => (
+  <header>
+    <div className="HeaderGroup">
+      <div className="HeaderSiteTitleContainer">
+        <h1>
+          <Link to="/" className="HeaderSiteTitle">
+            Shekhar Gurav
+          </Link>
+        </h1>
+      </div>
+      <nav className="HeaderSiteNav">
+        <Link to="/about" className="HeaderNavLink">
+          About
         </Link>
-      </h1>
+        <Link to="/work" className="HeaderNavLink">
+          Work
+        </Link>
+        <Link to="/writing" className="HeaderNavLink">
+          Writing
+        </Link>
+        <Link to="/cabinet" className="HeaderNavLink">
+          Cabinet
+        </Link>
+      </nav>
     </div>
   </header>
 )
